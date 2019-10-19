d4447c11a0ba568e3f4f42d456016e22bd75be13
teradonburi <daikiterai@gmail.com>
2019-10-20 08:27:51 +0900

    image audio

--- client/main.js
+++ client/main.js
@@ -1,7 +1,13 @@
 import 'regenerator-runtime/runtime'
 
 async function main() {
-
+  const audio = new Audio('./se_maoudamashii_onepoint15.mp3')
+  audio.controls = true
+  audio.addEventListener('ended', () => {
+    console.log('ended')
+    audio.play()
+  })
+  // audio.loop = true
+  document.body.appendChild(audio)
 }
 main()
-
